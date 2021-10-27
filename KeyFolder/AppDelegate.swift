c0634adfe2e38d7cb412a16b7f8540187bf46a22
tnantoka <tnantoka@bornneet.com>
2021-10-27 22:34:22 +0900

    chore: don't start admob on debug

--- KeyFolder/AppDelegate.swift
+++ KeyFolder/AppDelegate.swift
@@ -11,8 +11,11 @@ import GoogleMobileAds
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [GADSimulatorID];
+        #if DEBUG
+//            GADMobileAds.sharedInstance().start(completionHandler: nil)
+        #else
+            GADMobileAds.sharedInstance().start(completionHandler: nil)
+        #endif
 
         if let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
             let foldersURL = documentsURL.appendingPathComponent("folders")

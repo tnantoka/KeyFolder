37c705f582546d81ab28cd14226b49112d320aa6
tnantoka <tnantoka@bornneet.com>
2023-08-15 20:34:45 +0900

    feat: recreate project

--- KeyFolder/KeyFolderApp.swift
+++ KeyFolder/KeyFolderApp.swift
@@ -2,35 +2,16 @@
 //  KeyFolderApp.swift
 //  KeyFolder
 //
-//  Created by Tatsuya Tobioka on 2021/10/16.
+//  Created by Tatsuya Tobioka on 2023/08/15.
 //
 
 import SwiftUI
-import AppTrackingTransparency
 
 @main
 struct KeyFolderApp: App {
-    @Environment(\.scenePhase) private var scenePhase
-
-    @State private var isLocked = true
-    
-    let store = Store()
-
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                FolderListView(isLocked: $isLocked)
-            }
-            .onChange(of: scenePhase) { phase in
-                isLocked = true
-            }
-            .fullScreenCover(isPresented: $isLocked) {
-                PasscodeView(mode: Passcode().isConfigured ? .unlock : .initial, isLocked: $isLocked)
-            }
-            .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
-                ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in })
-            }
-            .environmentObject(store)
+            ContentView()
         }
     }
 }
